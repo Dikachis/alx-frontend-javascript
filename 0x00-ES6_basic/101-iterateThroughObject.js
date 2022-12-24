01307e94ce457f690e4a88a7b382bb8f5abcede7
Dikachis <dikachi015safety@gmail.com>
2022-12-24 04:58:35 +0100

    Added some files

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  let strin = '';
+  let _ = 0;
+  for (const objs of reportWithIterator) {
+    strin += objs;
+    _ += 1;
+    if (_ !== reportWithIterator.length) {
+      strin += ' | ';
+    }
+  }
+
+  return strin;
 }
