393a9567bb7aa00ddd5be58d1592d471b3d40a76
Dikachis <dikachi015safety@gmail.com>
2022-12-24 05:13:42 +0100

    fix some bugs

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let strin = '';
-  let _ = 0;
-  for (const objs of reportWithIterator) {
-    strin += objs;
-    _ += 1;
-    if (_ !== reportWithIterator.length) {
-      strin += ' | ';
-    }
-  }
-
-  return strin;
+  return reportWithIterator.join(' | ');
 }
